cb4f6dd4e401344b50b3aa4a911db8a61d6bddd4
BojanDabevski <bojan.dabevski@students.finki.ukim.mk>
2024-05-13 22:02:49 +0200

    initial commit

---
+++ src/main/java/diplomska/heatmail/HeatMailApplication.java
@@ -0,0 +1,13 @@
+package diplomska.heatmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeatMailApplication.class, args);
+    }
+
+}
