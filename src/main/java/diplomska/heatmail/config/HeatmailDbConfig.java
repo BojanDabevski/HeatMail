2c9fe705ec4c58aed9afc6bde180ce21b19a87f9
BojanDabevski <bojan.dabevski@students.finki.ukim.mk>
2024-06-03 22:23:50 +0200

    db and api config

--- src/main/java/diplomska/heatmail/config/HeatmailDbConfig.java
+++ src/main/java/diplomska/heatmail/config/HeatmailDbConfig.java
@@ -1,4 +1,22 @@
 package diplomska.heatmail.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Primary
+@Component
 public class HeatmailDbConfig {
+
+    @Bean("heatMailDataSource")
+    @ConfigurationProperties(prefix = "heatmail.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
